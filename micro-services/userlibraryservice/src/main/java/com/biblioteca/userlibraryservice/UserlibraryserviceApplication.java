7a02ea6adffc551caacc7d0cb311aa0a21a8ea86
Ayasha Hossain Jui <jui1801@cseku.ac.bd>
2025-10-03 22:13:46 +0600

    initialized userlibrary service

---
+++ micro-services/userlibraryservice/src/main/java/com/biblioteca/userlibraryservice/UserlibraryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.userlibraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserlibraryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserlibraryserviceApplication.class, args);
+	}
+
+}
